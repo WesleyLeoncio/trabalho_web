2468f6abacc994570f6566abb122e3d710e4b3c5
WESLEY LEONCIO <wesley.leoncio.izi@gmail.com>
2022-10-24 23:17:08 -0300

    Crud instrutor adicionado

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { application } from "express";
 import dotenv from "dotenv";
 import connectDatabase from "./config/db.js";
 import { errorHandling } from "./utils/error.js";
@@ -6,6 +6,8 @@ import { errorHandling } from "./utils/error.js";
 import routeAlunos from "./routes/routesAlunos.js";
 import routeGrupoMuscular from "./routes/routesGrupoMuscular.js";
 import routeTipoExercicio from "./routes/routesTipoExercicio.js";
+import routeInstrutor from "./routes/routesInstrutor.js";
+//import routeFicha from "./routes/routesFicha.js";
 
 const app = express();
 dotenv.config();
@@ -13,9 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(errorHandling);
 
-app.use("/api/grupoMuscular", routeGrupoMuscular);
-app.use("/api/tipoExercicio", routeTipoExercicio);
+app.use("/api/gruposmusculares", routeGrupoMuscular);
+app.use("/api/tiposexercicios", routeTipoExercicio);
 app.use("/api/alunos", routeAlunos);
+app.use("/api/instrutores", routeInstrutor);
+//app.use("/api/fichas",routeFicha);
 
 app.listen(8080, () => {
     connectDatabase();
