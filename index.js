a1046913599c976aedbfcc25ecef91e840debfe2
Wesley Notebook <wesley.leoncio.izi@gmail.com>
2022-10-25 18:20:33 -0300

    correção de fichas

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import routeGrupoMusculares from "./routes/routesGrupoMusculares.js";
 import routeTipoExercicios from "./routes/routesTipoExercicios.js";
 import routeInstrutores from "./routes/routesInstrutores.js";
 //import routeExercicios from "./routes/routesExercicios.js";
-//import routeFichas from "./routes/routesFicha.js";
+import routeFichas from "./routes/routesFichas.js";
 
 const app = express();
 dotenv.config();
@@ -21,7 +21,7 @@ app.use("/api/tiposexercicios", routeTipoExercicios);
 app.use("/api/alunos", routeAlunos);
 app.use("/api/instrutores", routeInstrutores);
 //app.use("/api/exercios", routeExercicios);
-//app.use("/api/fichas",routeFicha);
+app.use("/api/fichas", routeFichas);
 
 app.listen(8080, () => {
     connectDatabase();
