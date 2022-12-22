d6fa82d0dc64d6bdfe392578af4e637cbb0a42e8
Wesley <wesley.leoncio.izi@gmail.com>
2022-12-22 13:03:53 -0300

    Listagem e exclusão de instrutores

---
+++ frontend/src/components/TableInstrutores.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
+import ConfirmModal from "./ConfirmModal";
+import InformModal from "./InformModal";
+import { authHeader } from "../services/authServices";
+
+const TableInstrutores = ({ instrutores, setInstrutores }) => {
+     const [instrutorExcluir, setInstrutorExcluir] = useState(null);
+    const [modal, setModal] = useState(undefined);
+
+    function confirmarExclusao(instrutor) {
+        setInstrutorExcluir(instrutor);
+        const confirmModal = new bootstrap.Modal("#confirmModal", {});
+        setModal(confirmModal);
+        confirmModal.show();
+    }
+
+    function excluirInstrutor() {
+        axios
+            .delete(`http://localhost:8080/api/instrutores/${instrutorExcluir._id}`, { headers: authHeader() })
+            .then((data) => {
+                const instrutoresAtualizados = instrutores.filter((instrutor) => instrutor._id !== instrutorExcluir._id);
+                setInstrutores(instrutoresAtualizados);
+                modal.hide();
+                const informModal = new bootstrap.Modal("#informModal", {});
+                informModal.show();
+            })
+            .catch((error) => {
+                console.log(error);
+                modal.hide();
+            });
+    }
+
+    return instrutores.length === 0 ? (
+        <div className="alert alert-info">Nenhum instrutor cadastrado</div>
+    ) : (
+        <>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Data Nascimento</th>
+                        <th>E-mail</th>
+                        <th>Sexo</th>
+                        <th>Situação</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {instrutores.map((instrutor) => (
+                        <tr key={instrutor._id}>
+                            <td>{instrutor.nome}</td>
+                            <td>{new Date(instrutor.dataNascimento.substring(0, 10) + "T12:00:00").toLocaleDateString()}</td>
+                            <td>{instrutor.email}</td>
+                            <td>{instrutor.sexo === "M" ? "Masculino" : "Feminino"}</td>
+                            <td>{instrutor.ativo ? "Ativo" : "Inativo"}</td>
+                            <td>
+                                <Link className="btn btn-sm btn-warning me-1" to={`/instrutores/alterar/${instrutor._id}`}>
+                                    <i className="bi bi-pen"></i>
+                                </Link>
+                                <button className="btn btn-sm btn-danger" onClick={() => confirmarExclusao(instrutor)}>
+                                    <i className="bi bi-trash"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <ConfirmModal question={`Deseja realmente excluir o instrutor <b>${instrutorExcluir?.nome}</b>?`} action={excluirInstrutor} />
+            <InformModal info={`Instrutor <b>${instrutorExcluir?.nome}</b> excluído com sucesso.`} />
+        </>
+    );
+};
+
+export default TableInstrutores;
--- frontend/src/pages/instrutores/Listagem.js
+++ frontend/src/pages/instrutores/Listagem.js
@@ -1,11 +1,41 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Loading from "../../components/Loading";
+import TableInstrutores from "../../components/TableInstrutores";
+import { authHeader } from "../../services/authServices";
 import "./Listagem.css";
 
 
 const Listagem = () => {
+    const [instrutores, setInstrutores] = useState([]);
+    const [loading, setLoading] = useState(true);
 
+    const carregarInstrutores = () => {
+        axios
+            .get("http://localhost:8080/api/instrutores", { headers: authHeader() })
+            .then((response) => {
+                setInstrutores(response.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    useEffect(() => {
+        carregarInstrutores();
+    }, []);
     return (
         <>
-            <div>instrutores</div>
+           <div className="d-flex justify-content-between align-items-center">
+                <h1>Instrutores</h1>
+                <Link to="/instrutores/cadastrar" className="btn btn-primary">
+                    Novo
+                </Link>
+            </div>
+            <hr />
+            {loading ? <Loading /> : <TableInstrutores instrutores={instrutores} setInstrutores={setInstrutores} />}
         </>
     );
 };
